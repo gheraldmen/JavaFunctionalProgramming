8bfed053569beef3fdd444548df53af0884e6db5
dintry1 <gheraldmen5@gmail.com>
2019-08-31 15:38:26 +0800

    Update TotalSalesEachBranch

--- java-functional-programming/src/TotalSalesEachBranch.java
+++ java-functional-programming/src/TotalSalesEachBranch.java
@@ -1,6 +1,9 @@
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,51 +12,30 @@ import java.io.IOException;
 
 public class TotalSalesEachBranch {
     public static void main(String[] args) throws IOException {
-        getDavaoTotalSales();
-        getCebuTotalSales();
-        getManilaTotalSales();
-    }
-    //sorry po sir mali po yung solution ko.
-    public static void getDavaoTotalSales() throws IOException {
-        Path davaoPath = Paths.get("sales/davao.csv");
-        try (Stream<String> stream = Files.lines(davaoPath)) {
-            List<List<String>> values = stream.map(line -> Arrays.asList(line.split(","))).collect(Collectors.toList());
-            Double sales = values.stream()
-                    .map(i -> Double.parseDouble(i.get(2)) * Double.parseDouble(i.get(3)))
-                    .reduce(0.0, Double::sum);
-
-            System.out.println("Davao Total Sales: " + sales);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+                getTotalSalesEachBranch();
     }
 
-    public static void getCebuTotalSales() throws IOException {
-        Path cebuPath = Paths.get("sales/cebu.csv");
-        try (Stream<String> stream = Files.lines(cebuPath)) {
-            List<List<String>> values = stream.map(line -> Arrays.asList(line.split(","))).collect(Collectors.toList());
-            Double sales = values.stream()
-                    .map(i -> Double.parseDouble(i.get(2)) * Double.parseDouble(i.get(3)))
-                    .reduce(0.0, Double::sum);
-
-            System.out.println("Cebu Total Sales: " + sales);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void getManilaTotalSales() throws IOException {
+    public static void getTotalSalesEachBranch() throws IOException {
         Path manilaPath = Paths.get("sales/manila.csv");
-        try (Stream<String> stream = Files.lines(manilaPath)) {
-            List<List<String>> values = stream.map(line -> Arrays.asList(line.split(","))).collect(Collectors.toList());
-            Double sales = values.stream()
-                    .map(i -> Double.parseDouble(i.get(2)) * Double.parseDouble(i.get(3)))
-                    .reduce(0.0, Double::sum);
+        Path cebuPath = Paths.get("sales/cebu.csv");
+        Path davaoPath = Paths.get("sales/davao.csv");
 
-            System.out.println("Manila Total Sales: " + sales);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        Stream<List> stream = Stream.of(
+                List.of("Manila Total Sales", getEachTotalSales(manilaPath)),
+                List.of("Cebu Total Sales", getEachTotalSales(cebuPath)),
+                List.of("Davao Total Sales", getEachTotalSales(davaoPath)));
+        stream.forEach(System.out::println);
     }
+
+    public static BigDecimal getEachTotalSales(Path eachPath) throws IOException {
+
+        Stream<String> stream = Files.lines(eachPath);
+        List<List<String>> values = stream.map(line -> Arrays.asList(line.split(","))).collect(Collectors.toList());
+       BigDecimal sales= values.stream()
+                .map(i -> new ItemModel((i.get(0)), LocalDate.parse(i.get(1), DateTimeFormatter.ofPattern("M/d/yyyy")), Integer.parseInt(i.get(2)), new BigDecimal(i.get(3))))
+                .map(i->i.getPrice().multiply(BigDecimal.valueOf(i.getUnitSold())))
+                .reduce(BigDecimal.valueOf(0), BigDecimal::add);
+       return sales;
+    }
+
 }
